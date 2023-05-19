2988f1373bad39b82e2a2690cd4c091831c3ba76
Nemesix493 <verquerre.kevin@gmail.com>
2023-05-19 19:08:04 +0200

    style (performance_test): remove useless response variable

--- tests/performance_test/locustfile.py
+++ tests/performance_test/locustfile.py
@@ -5,15 +5,15 @@ from tests.run_test_server import clubs, competitions
 class ProjectPerfTest(HttpUser):
     @task
     def index(self):
-        response = self.client.get('')
+        self.client.get('')
     
     @task(2)
     def clubs_point(self):
-        response = self.client.get('/clubs-points')
+        self.client.get('/clubs-points')
     
     @task
     def show_summary(self):
-        response = self.client.post(
+        self.client.post(
             '/showSummary',
             data = {
                 'email': clubs[0]['email']
@@ -22,7 +22,7 @@ class ProjectPerfTest(HttpUser):
     
     @task
     def purchase_places(self):
-        response = self.client.post(
+        self.client.post(
             '/purchasePlaces',
             data = {
                 'club': clubs[0]['name'],
