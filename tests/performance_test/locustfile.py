4cbee59d4977fa497540a9ea065f6f6df4e6f875
Nemesix493 <verquerre.kevin@gmail.com>
2023-05-19 19:16:30 +0200

    style : to fit with PEP

--- tests/performance_test/locustfile.py
+++ tests/performance_test/locustfile.py
@@ -2,29 +2,30 @@ from locust import HttpUser, task
 
 from tests.run_test_server import clubs, competitions
 
+
 class ProjectPerfTest(HttpUser):
     @task
     def index(self):
         self.client.get('')
-    
+
     @task(2)
     def clubs_point(self):
         self.client.get('/clubs-points')
-    
+
     @task
     def show_summary(self):
         self.client.post(
             '/showSummary',
-            data = {
+            data={
                 'email': clubs[0]['email']
             }
         )
-    
+
     @task
     def purchase_places(self):
         self.client.post(
             '/purchasePlaces',
-            data = {
+            data={
                 'club': clubs[0]['name'],
                 'competition': competitions[0]['name'],
                 'numberOfPlaces': 1
